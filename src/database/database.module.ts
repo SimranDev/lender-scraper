d7f2a038f2cd6b2b7d183ebbaf5e8b2838c302e6
Simran <150635196+simran-cropsy@users.noreply.github.com>
2025-01-11 17:24:52 +1300

    feat(database): integrate lenders and products schemas into the database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,6 +3,8 @@ import { DATABASE_CONNECTION } from './database-connection'
 import { ConfigService } from '@nestjs/config'
 import { Pool } from 'pg'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import * as lenderSchema from '../lenders/schema'
+import * as productSchema from '../products/schema'
 
 @Module({
   imports: [],
@@ -12,14 +14,18 @@ import { drizzle } from 'drizzle-orm/node-postgres'
       provide: DATABASE_CONNECTION,
       useFactory: (configService: ConfigService) => {
         const pool = new Pool({
-          connectionString: configService.getOrThrow('DATABASE_URL'),
+          connectionString: configService.getOrThrow('DB_URL'),
         })
         return drizzle(pool, {
-          schema: {},
+          schema: {
+            ...lenderSchema,
+            ...productSchema,
+          },
         })
       },
       inject: [ConfigService],
     },
   ],
+  exports: [DATABASE_CONNECTION],
 })
 export class DatabaseModule {}
