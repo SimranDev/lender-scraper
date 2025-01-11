464014b1a3131c815e5f76d593cb7cf722737c2d
Simran <150635196+simran-cropsy@users.noreply.github.com>
2025-01-11 15:25:16 +1300

    feat: update Drizzle configuration for PostgreSQL with detailed dbCredentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'drizzle-kit'
+import { readFileSync } from 'fs'
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/**/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { ca: readFileSync('.cert/ap-southeast-2-bundle.pem').toString() },
   },
 })
