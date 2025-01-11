5829e6908edec9b8a5c30606c19390e6bb8ce85a
Simran <150635196+simran-cropsy@users.noreply.github.com>
2025-01-11 17:24:46 +1300

    feat: add ProductsModule to the main application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from './database/database.module'
 import { ConfigModule } from '@nestjs/config'
 import { LendersModule } from './lenders/lenders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, LendersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, LendersModule, ProductsModule],
   controllers: [],
   providers: [],
 })
