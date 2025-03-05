401df2a9a80d8f322cd45bd9ea5f00a0dcdcbd44
Simran <150635196+simran-cropsy@users.noreply.github.com>
2025-03-06 12:37:47 +1300

    feat(drizzle): simplify database configuration by using DB_URL and commenting out legacy credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,20 @@
+import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
-import { readFileSync } from 'fs'
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/**/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: { ca: readFileSync('.cert/ap-southeast-2-bundle.pem').toString() },
+    url: process.env.DB_URL!,
   },
 })
+
+// dbCredentials: {
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   ssl: { ca: readFileSync('.cert/ap-southeast-2-bundle.pem').toString() },
+// },
